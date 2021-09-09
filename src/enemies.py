825d2ac4132f1f5d6d51eacbaa6e21c5dcf0eb0e
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-09-09 17:35:42 +0300

    add uniform_cost_search

--- src/enemies.py
+++ src/enemies.py
@@ -37,7 +37,7 @@ class Spirit(pygame.sprite.Sprite, Entity):
     def change_direction(self, player_i, player_j):
 
         # direction = random.choice(self.get_available_directions())
-        direction = self.deep_first_search(player_i, player_j)
+        direction = self.uniform_cost_search(player_i, player_j)
         if direction == "left":
             self.change_x = -2
             self.change_y = 0
@@ -100,8 +100,37 @@ class Spirit(pygame.sprite.Sprite, Entity):
             else:
                 next_nodes_to_visit = next_nodes_to_visit[:-1]
 
-    def uniform_cost_search(self):
-        pass
+    def uniform_cost_search(self, want_i, want_j):
+        # TODO: remove tube
+        j = self.rect.topleft[0] // BLOCK_SIZE
+        i = self.rect.topleft[1] // BLOCK_SIZE
+        visited = {(i, j)}
+        prices = dict()
+
+        for node_to_visit_i, node_to_visit_j, direction in get_available_directions_coordinates(self.grid, i, j):
+            prices[(node_to_visit_i, node_to_visit_j)] = [1, direction]
+
+        while len(prices) != 0:
+            cheapest_node = list(prices.keys())[0]
+            cheapest_price = prices[cheapest_node][0]
+
+            for key, value in prices.items():
+                if value[0] < cheapest_price:
+                    cheapest_price = value[0]
+                    cheapest_node = key
+
+            visit_i, visit_j = cheapest_node
+            direction = prices[cheapest_node][1]
+            del prices[cheapest_node]
+            if not (visit_i, visit_j) in visited:
+                visited.add((visit_i, visit_j))
+                if visit_i == want_i and visit_j == want_j:
+                    return direction
+                for node_to_visit_i, node_to_visit_j, _ in get_available_directions_coordinates(self.grid, visit_i,
+                                                                                                visit_j):
+                    if (node_to_visit_i, node_to_visit_j) not in prices or \
+                            prices[(node_to_visit_i, node_to_visit_j)][0] > cheapest_price + 1:
+                        prices[(node_to_visit_i, node_to_visit_j)] = [cheapest_price + 1, direction]
 
 
 def get_available_directions_coordinates(grid, i, j):
